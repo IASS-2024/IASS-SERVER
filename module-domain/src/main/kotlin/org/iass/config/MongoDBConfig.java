a48c8b86a2e3e6761f229fbce1cf3b941081a6ba
its-sky <smc9919@naver.com>
2024-05-02 18:15:42 +0900

    [Feat] #5 - BaseTimeEntity 구현

--- module-domain/src/main/kotlin/org/iass/config/MongoDBConfig.java
+++ module-domain/src/main/kotlin/org/iass/config/MongoDBConfig.java
@@ -3,9 +3,11 @@ package org.iass.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoAuditing
 @EnableAutoConfiguration(exclude = MongoAutoConfiguration.class)
 @EnableMongoRepositories(basePackages = "org.iass")
 public class MongoDBConfig {
