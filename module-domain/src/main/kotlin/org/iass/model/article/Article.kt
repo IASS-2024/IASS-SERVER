52693878e5acac097667786e3f4f228629bd0c5c
its-sky <smc9919@naver.com>
2024-05-04 19:09:36 +0900

    [Refactor] #5 - Article BaseTimeEntity 상속 추가

--- module-domain/src/main/kotlin/org/iass/model/article/Article.kt
+++ module-domain/src/main/kotlin/org/iass/model/article/Article.kt
@@ -1,6 +1,7 @@
 package org.iass.model.article
 
 import jakarta.persistence.Id
+import org.iass.model.common.BaseTimeEntity
 import org.iass.model.user.User
 import org.springframework.data.mongodb.core.mapping.Document
 
@@ -11,18 +12,18 @@ class Article(
 	keyword: String,
 	val user: User,
 	val week: Int
-) {
+) : BaseTimeEntity() {
 	@Id
 	var id: String? = null
 
 	var url = url
-		private set
+		protected set
 
 	var description = description
-		private set
+		protected set
 
 	var keyword = keyword
-		private set
+		protected set
 
 	fun modifyKeyword(keyword: String) {
 		this.keyword = keyword
