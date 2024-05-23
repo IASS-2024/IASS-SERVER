5d95766f864af2faba3e17594989bf73085d5190
its-sky <smc9919@naver.com>
2024-05-24 00:10:37 +0900

    [Feat] #9 - 공통 API Response DTO 구현

---
+++ module-common/src/main/kotlin/org/iass/dto/response/ApiResponse.kt
@@ -0,0 +1,43 @@
+package org.iass.dto.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("status", "code", "message", "data")
+data class ApiResponse<T>
+	@JvmOverloads
+	constructor(
+		val status: Int,
+		val code: Int,
+		val message: String,
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		val data: T? = null
+	) {
+		companion object {
+			@JvmStatic
+			fun success(successType: SuccessType): ApiResponse<Nothing> {
+				return ApiResponse(successType.status.value(), successType.code, successType.message)
+			}
+
+			@JvmStatic
+			fun <T> success(
+				successType: SuccessType,
+				data: T
+			): ApiResponse<T> {
+				return ApiResponse(successType.status.value(), successType.code, successType.message, data)
+			}
+
+			@JvmStatic
+			fun error(errorType: ErrorType): ApiResponse<Nothing> {
+				return ApiResponse(errorType.status.value(), errorType.code, errorType.message)
+			}
+
+			@JvmStatic
+			fun <T> error(
+				errorType: ErrorType,
+				data: T
+			): ApiResponse<T> {
+				return ApiResponse(errorType.status.value(), errorType.code, errorType.message, data)
+			}
+		}
+	}
