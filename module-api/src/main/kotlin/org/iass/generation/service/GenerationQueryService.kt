cb12d186d15bb20713eca0742a72b43dbc01ba7a
its-sky <smc9919@naver.com>
2024-05-23 23:26:29 +0900

    [Refactor] #9 - Generation 엔티티 인터페이스 구현 방식으로 변경

--- module-api/src/main/kotlin/org/iass/generation/service/GenerationQueryService.kt
+++ module-api/src/main/kotlin/org/iass/generation/service/GenerationQueryService.kt
@@ -1,8 +1,3 @@
 package org.iass.generation.service
 
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-
-@Service
-@Transactional(readOnly = true)
-class GenerationQueryService
+interface GenerationQueryService
