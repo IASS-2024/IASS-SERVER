db8771095e36de6b7175d5c99216989982bb8867
its-sky <smc9919@naver.com>
2024-05-28 23:21:26 +0900

    [Fix] #11 - GenerationRepository ID type 오류 수정

--- module-domain/src/main/kotlin/org/iass/repository/generation/GenerationRepository.kt
+++ module-domain/src/main/kotlin/org/iass/repository/generation/GenerationRepository.kt
@@ -5,4 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GenerationRepository : MongoRepository<Generation, Long>
+interface GenerationRepository : MongoRepository<Generation, String>
