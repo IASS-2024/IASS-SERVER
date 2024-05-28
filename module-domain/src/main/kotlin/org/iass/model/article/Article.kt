3597113e841210d910fc846bd6285f6f9d0d5351
its-sky <smc9919@naver.com>
2024-05-28 23:24:52 +0900

    [Refactor] #11 - User 엔티티 개발 전 임시로 String 타입으로 변경

--- module-domain/src/main/kotlin/org/iass/model/article/Article.kt
+++ module-domain/src/main/kotlin/org/iass/model/article/Article.kt
@@ -2,7 +2,6 @@ package org.iass.model.article
 
 import jakarta.persistence.Id
 import org.iass.model.common.BaseTimeEntity
-import org.iass.model.user.User
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("article")
@@ -10,7 +9,7 @@ class Article(
 	url: String,
 	description: String,
 	keyword: String,
-	val user: User,
+	val user: String,
 	val week: Int
 ) : BaseTimeEntity() {
 	@Id
