31eedbe8d4e59d0589c69f1b22ad653e49f505f8
Choi YoungLyn <joeuni0129@gmail.com>
2024-06-13 22:55:54 +0900

    [Add] #13 - FeignClient, JWT 의존성 추가

--- module-api/build.gradle.kts
+++ module-api/build.gradle.kts
@@ -22,4 +22,19 @@ dependencies {
 
 	// Validation
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	//Redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis:3.2.1")
+
+	//JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	//Logging
+	implementation ("io.github.oshai:kotlin-logging-jvm:6.0.9")
+
+	//Security
+	implementation ("org.springframework.boot:spring-boot-starter-security")
+
 }
--- module-external/build.gradle.kts
+++ module-external/build.gradle.kts
@@ -8,4 +8,13 @@ tasks.getByName("jar") {
 
 dependencies {
 	implementation(project(":module-common"))
+
+	// openfeign
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
+
+	//JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
 }
